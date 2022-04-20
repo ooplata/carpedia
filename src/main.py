9d0070949c2172119abddb4713a59eb16b65fe66
ooplata <99055524+ooplata@users.noreply.github.com>
2022-04-20 16:30:49 -0500

    Final entry point for the time being

--- src/main.py
+++ src/main.py
@@ -1,2 +1,17 @@
-val = input("Hello! What would you like to have in your dream car?\n")
-print("\nPerfect! Thanks to this project, you will be able to get cars that have " + val + " and more.")
+from cars.cars import ImportedCar
+from cars.engine import Engine
+from cars.enums.enums import FuelType, InductionType
+from cars.internals import Internals
+
+
+carEngine = Engine(4 , 4 , InductionType.super , 2 , "Torque")
+
+carInternals = Internals(carEngine , "Manual" , "Open" , "Front" ,
+FuelType.gasoline , 6.5)
+
+car = ImportedCar(2022 , "USA" , 2022 , "Mito" , "AlfaRomeo" , "Compact"
+, 0, carInternals)
+
+val = input("What would you like to see in an imported car?\n")
+print(F"Check out this amazing imported car!\n\n{car.model} from {car.brand} " +
+F"({car.releaseYear})\nImported in {car.importedOn} from {car.originCountry}")
