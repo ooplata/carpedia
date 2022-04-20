3068535f588595df4b6f4a4ee83ed96552468437
ooplata <99055524+ooplata@users.noreply.github.com>
2022-04-20 16:11:41 -0500

    Fixed cars constructors

--- src/cars/cars.py
+++ src/cars/cars.py
@@ -1,27 +1,37 @@
 from abc import ABC
-import internals
 
-#Definición de las clases
+
+# Definición de las clases
 class Car(ABC):
-    def __init__(self, releaseYear, model, brand, body, priceHistory, internals):
+    def __init__(self, releaseYear, model, brand, body, priceHistory,
+    internals):
         self.releaseYear = releaseYear
         self.model = model
         self.brand = brand
         self.body = body
         self.priceHistory = priceHistory
-        self.internals = Internals
+        self.internals = internals
 
 class ImportedCar(Car):
-    def __init__(self, importedOn, originCountry):
+    def __init__(self, importedOn, originCountry, releaseYear, model, brand,
+        body, priceHistory, internals):
         self.importedOn = importedOn
         self.originCountry = originCountry
+        super().__init__(releaseYear, model, brand, body, priceHistory,
+        internals)
 
 class NewCar(Car):
-    def __init__(self, dealership):
+    def __init__(self, dealership, releaseYear, model, brand, body,
+    priceHistory, internals):
         self.dealership = dealership
+        super().__init__(releaseYear, model, brand, body, priceHistory,
+        internals)
 
 class UsedCar(Car):
-    def __init__(self, mileage, ownerCount, modifications):
+    def __init__(self, mileage, ownerCount, modifications, releaseYear, model,
+    brand, body, priceHistory, internals):
         self.mileage = mileage
         self.ownerCount = ownerCount
         self.modifications = modifications
+        super().__init__(releaseYear, model, brand, body, priceHistory,
+        internals)
