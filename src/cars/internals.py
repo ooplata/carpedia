e91ebbcc47cfcb7cd3d475c53d2345a9cd615625
Omar Plata Salas <99055524+ooplata@users.noreply.github.com>
2022-04-28 13:47:47 -0500

    Updated property name

--- src/cars/internals.py
+++ src/cars/internals.py
@@ -8,7 +8,7 @@ class Internals:
     def __init__(self, json):
         self.engine = Engine(json["engine"])
         self.transmission = json["transmission"]
-        self.diff = json["4WD"]
+        self.fourWd = json["4WD"]
         self.powerTrain = json["powerTrain"]
         self.fuelType = json["fuelType"]
         self.fuelConsumption = json["fuelConsumption"]
