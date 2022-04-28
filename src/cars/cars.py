cc21a499f462f4c2053f3e12b3eebc22ed447b40
ooplata <ooplata@uninorte.edu.co>
2022-04-27 20:55:25 -0500

    PEP8 compliance

--- src/cars/cars.py
+++ src/cars/cars.py
@@ -4,7 +4,7 @@ from abc import ABC
 # Definición de las clases
 class Car(ABC):
     def __init__(self, releaseYear, model, brand, body, priceHistory,
-    internals):
+                 internals):
         self.releaseYear = releaseYear
         self.model = model
         self.brand = brand
@@ -12,26 +12,29 @@ class Car(ABC):
         self.priceHistory = priceHistory
         self.internals = internals
 
+
 class ImportedCar(Car):
     def __init__(self, importedOn, originCountry, releaseYear, model, brand,
-        body, priceHistory, internals):
+                 body, priceHistory, internals):
         self.importedOn = importedOn
         self.originCountry = originCountry
         super().__init__(releaseYear, model, brand, body, priceHistory,
-        internals)
+                         internals)
+
 
 class NewCar(Car):
     def __init__(self, dealership, releaseYear, model, brand, body,
-    priceHistory, internals):
+                 priceHistory, internals):
         self.dealership = dealership
         super().__init__(releaseYear, model, brand, body, priceHistory,
-        internals)
+                         internals)
+
 
 class UsedCar(Car):
     def __init__(self, mileage, ownerCount, modifications, releaseYear, model,
-    brand, body, priceHistory, internals):
+                 brand, body, priceHistory, internals):
         self.mileage = mileage
         self.ownerCount = ownerCount
         self.modifications = modifications
         super().__init__(releaseYear, model, brand, body, priceHistory,
-        internals)
+                         internals)
