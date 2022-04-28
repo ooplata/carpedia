c49e972f36fc55e3da667f1bd8c7fa3130a9100e
ooplata <ooplata@uninorte.edu.co>
2022-04-27 20:53:06 -0500

    Added simple JSON deserialization

--- src/main.py
+++ src/main.py
@@ -1,16 +1,15 @@
+# %%
+import json
 from cars.cars import ImportedCar
 from cars.engine import Engine
 from cars.internals import Internals
 
+# %%
+file = open('Cars.json')
+data = json.load(file)
 
-carEngine = Engine(4 , 4 , "Super" , 2 , "Torque")
+for car in data["Imported"]:
+    print(car)
 
-carInternals = Internals(carEngine , "Manual" , "Open" , "Front" ,
-"Gasoline" , 6.5)
-
-car = ImportedCar(2022 , "USA" , 2022 , "Mito" , "AlfaRomeo" , "Compact"
-, 0, carInternals)
-
-val = input("What would you like to see in an imported car?\n")
-print(F"Check out this amazing imported car!\n\n{car.model} from {car.brand} " +
-F"({car.releaseYear})\nImported in {car.importedOn} from {car.originCountry}")
+file.close()
+# %%
