da2d47ed24cb0b22f5496ae766b0f33a33642766
ooplata <99055524+ooplata@users.noreply.github.com>
2022-05-11 04:51:10 -0500

    Added simple price graphs

--- src/main.py
+++ src/main.py
@@ -1,14 +1,29 @@
 # %%
 import json
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
 from cars.cars import ImportedCar
 
 # %%
 file = open('Cars.json')
 data = json.load(file)
 
+# %%
 for car in data["Imported"]:
     c = ImportedCar(car)
-    print(c.model)
 
+    prices = c.priceHistory.items()
+    x, y = zip(*prices)
+    plt.plot(x, y)
+    plt.xlabel('Year')
+    plt.ylabel('Price')
+    plt.title(c.model)
+    plt.show()
+
+# %%
 file.close()
+
 # %%
