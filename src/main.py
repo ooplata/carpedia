cb91952a95c382feb283808c7262aca78051d2a7
ooplata <99055524+ooplata@users.noreply.github.com>
2022-05-25 21:35:02 -0500

    Improved project structure

--- src/main.py
+++ src/main.py
@@ -4,24 +4,36 @@ import json
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from pydoc import importfile
+from tkinter import *
 
 from cars.cars import ImportedCar
 
 # %%
-file = open('Cars.json')
+file = open('assets/Cars.json')
 data = json.load(file)
+
+cars = []
 
 # %%
 for car in data["Imported"]:
     c = ImportedCar(car)
-
-    prices = c.priceHistory.items()
-    x, y = zip(*prices)
-    plt.plot(x, y, marker='o')
-    plt.xlabel('Year')
-    plt.ylabel('Price')
-    plt.title(c.model)
-    plt.show()
+    cars.append(c)
 
 # %%
 file.close()
+
+# %%
+root = Tk()
+root.geometry("800x800")
+
+# Title label widget
+titleScreen = Label(root, text="Carpedia")
+titleScreen.pack()
+
+# Carpedia logo widget
+logo = PhotoImage(file='assets/logo.gif')
+logoLabel = Label(root, image=logo)
+logoLabel.pack()
+
+root.mainloop()
