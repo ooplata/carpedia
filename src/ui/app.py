27807933abaf7065e513894d9e5adfb88d4bc565
ooplata <99055524+ooplata@users.noreply.github.com>
2022-05-26 09:04:56 -0500

    Added dervied app class

---
+++ src/ui/app.py
@@ -0,0 +1,32 @@
+# %%
+from cars.cars import ImportedCar
+from ui.tkcore import Application
+
+import json
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from tkinter import *
+
+
+# %%
+class App(Application):
+    def __init__(self) -> None:
+        super().__init__("Carpedia", "assets/logo.gif", "600x600")
+
+    def onstartup(self):
+        file = open('assets/Cars.json')
+        data = json.load(file)
+
+        cars = []
+        for car in data["Imported"]:
+            c = ImportedCar(car)
+            cars.append(c)
+
+        file.close()
+        lb = Listbox(self.window)
+        for car in cars:
+            lb.insert(END, car)
+        lb.pack()
+
+# %%
