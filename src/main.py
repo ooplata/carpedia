3aa3e415a1db72ff266169c5748163dd0ca54023
ooplata <99055524+ooplata@users.noreply.github.com>
2022-05-26 09:05:29 -0500

    Cleaned up entry point

--- src/main.py
+++ src/main.py
@@ -1,39 +1,8 @@
 # %%
-import json
-
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-from pydoc import importfile
-from tkinter import *
-
-from cars.cars import ImportedCar
+from ui.app import App
 
 # %%
-file = open('assets/Cars.json')
-data = json.load(file)
-
-cars = []
+app = App()
+app.run()
 
 # %%
-for car in data["Imported"]:
-    c = ImportedCar(car)
-    cars.append(c)
-
-# %%
-file.close()
-
-# %%
-root = Tk()
-root.geometry("600x600")
-
-# Title label widget
-titleScreen = Label(root, text="Carpedia")
-titleScreen.pack()
-
-# Carpedia logo widget
-logo = PhotoImage(file='assets/logo.gif')
-logoLabel = Label(root, image=logo)
-logoLabel.pack()
-
-root.mainloop()
