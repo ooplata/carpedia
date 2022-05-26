8e0861c1223206cf91720a3eda269dd0b1f987ae
ooplata <99055524+ooplata@users.noreply.github.com>
2022-05-26 08:48:00 -0500

    Added base Application class

---
+++ src/ui/tkcore.py
@@ -0,0 +1,23 @@
+# %%
+from abc import ABC, abstractmethod
+from tkinter import *
+
+
+# %%
+class Application(ABC):
+    def __init__(self, title: str, iconPath: str,
+                 geometry: str) -> None:
+        self.window = Tk()
+        self.window.title(title)
+        self.window.iconbitmap(iconPath)
+        self.window.geometry(geometry)
+
+    def run(self):
+        self.onstartup()
+        self.window.mainloop()
+
+    @abstractmethod
+    def onstartup(self):
+        ...
+
+# %%
